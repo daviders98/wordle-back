77516774a101dfababa3affa305ade5efcc7d593
David Garcia <davidalexysg@gmail.com>
2025-11-05 21:54:00 -0700

    fix(WOP-17): Change url on new endpoint

--- server/urls.py
+++ server/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import guess_word, get_jwt, health_check, list_words
 
 urlpatterns = [
-    path('guess_word/', guess_word),
+    path('guess-word/', guess_word),
     path('get-jwt/', get_jwt),
     path("health/", health_check),
     path("list/",list_words)
