4ad2dc52d00054ee5eb076daabe5367d870bc312
David Garcia <davidalexysg@gmail.com>
2025-11-15 12:25:03 -0700

    feat(WOP-23): Add 404

--- server/urls.py
+++ server/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from django.http import JsonResponse
 from .views import guess_word, get_jwt, health_check, list_words
+
+def custom_404(request, exception):
+    return JsonResponse({"error": "Endpoint not found"}, status=404)
 
 urlpatterns = [
     path('guess-word/', guess_word),
     path('get-jwt/', get_jwt),
     path("health/", health_check),
-    path("list/",list_words)
+    path("list/", list_words),
 ]
+
+handler404 = custom_404
